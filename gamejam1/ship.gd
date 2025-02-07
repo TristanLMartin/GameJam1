764263608b68e25f70a7f753f7bb228d2941a81a
TristanLMartin <tristanlmartin95@gmail.com>
2025-02-07 15:50:23 -0800

    player controlled directions

--- gamejam1/ship.gd
+++ gamejam1/ship.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
-@export var SPEED := 100;
+@export var SPEED := 10;
 @onready var path_to_follow : PathFollow2D = %PathFollow2D
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	path_to_follow.progress += 1
 	pass
+	
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("Move_Left", "Move_Right", "Move_Up", "Move_Down")
+	
+	if direction.x > 0:
+		path_to_follow.progress += SPEED
+	elif direction.x < 0:
+		path_to_follow.progress -= SPEED
+	
