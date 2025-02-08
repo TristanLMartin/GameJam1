58eaa15f27c9ad91f93308b9ecfe8d0c8c4d6203
Brandon Hofbauer <71418632+brandonhofbauer@users.noreply.github.com>
2025-02-08 00:42:01 -0800

    Fixed bullet collisions on enemy
    Be wary of area vs characterbody and etc

--- gamejam1/bullet.gd
+++ gamejam1/bullet.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 		queue_free()
 
 
-func _on_area_entered(area: Area2D) -> void:
-	print(area.name)
-	area.on_hit()
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	# area.on_hit()
 	queue_free()
