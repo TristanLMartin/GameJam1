dcd6c9a3ad12ae62681e1125da47d0625d4b2337
Brandon Hofbauer <71418632+brandonhofbauer@users.noreply.github.com>
2025-02-07 22:47:21 -0800

    Starting the pew pew pew

--- gamejam1/bullet.gd
+++ gamejam1/bullet.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-@export var bullet_speed = 5
+@export var SPEED : int = 300
+var RANGE : int = 1200
+var travelled_distance : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.y += bullet_speed
+	var direction : Vector2 = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
+	travelled_distance += SPEED * delta
+	
+	if travelled_distance > RANGE:
+		queue_free()
