4c69f0c7f3f0d493220f07898c9d2d5ecdca9aff
ShyBry <158124177+ShyBry@users.noreply.github.com>
2025-02-08 13:20:02 -0800

    CowCooldownSkeleton

--- gamejam1/ship.gd
+++ gamejam1/ship.gd
@@ -8,10 +8,12 @@ var bullet_scene
 
 func _ready() -> void:
 	bullet_scene = preload("res://bullet.tscn")
+	$CowTimer.timeout.connect(_on_CowTimer_timeout)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Shoot") and bullet_timer.is_stopped():
 		shoot()
+	$CowTimer.wait_time = cow_generation_time
 	if Input.is_action_just_pressed("Place_Cow"):
 		place_cows_on_planet()
 	
@@ -33,11 +35,16 @@ func shoot() -> void:
 	bullet_instance.global_position = %PathFollow2D.global_position * 1.2
 	add_child(bullet_instance)
 
-
-@export var has_cows_unlocked = true #If you have the upgrade or not
-@export var cow_count = 10 #How many cows you have in your "inventory"
+#When has_cows_unlocked upgrade becomes true, we need to $CowTimer.start()
+@export var has_cows_unlocked = false #If you have the upgrade or not
+@export var cow_count = 0 #How many cows you have in your "inventory"
+#When cows_placed++ and cow_count isn't a denomination, we need to resolve...
 @export var cows_placed = 1 #How many cows you place per press
+@export var cow_generation_time = 10.0
 signal cow_signal(amount)
+
+func _on_CowTimer_timeout() -> void:
+	cow_count += cows_placed
 
 func place_cows_on_planet() -> void:
 	if has_cows_unlocked == true and cow_count >= cows_placed: #Checks if you have the cow upgrade and have cows off cooldown
