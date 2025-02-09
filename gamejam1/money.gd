4fbc0599dec019bc6bcea89430b0d16d6ea56c13
TristanLMartin <tristanlmartin95@gmail.com>
2025-02-09 13:24:03 -0800

    Money Labels

---
+++ gamejam1/money.gd
@@ -0,0 +1,15 @@
+extends Label
+var value = 0
+
+func _ready() -> void:
+	self.text = str(value)
+	%Timer.start()
+
+func _process(delta: float) -> void:
+	self.text = str(value)
+
+
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
