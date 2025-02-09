4ba39e4096efc2cc784c839dd6e4bcb5f0f39a66
Chris McCarthy <mccarthy.leechris@gmail.com>
2025-02-08 18:00:07 -0800

    new aliens created

--- gamejam1/alien2.gd
+++ gamejam1/alien2.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 @onready var wave_spawner = get_node('/root/Main/WaveSpawner')
 var attacking = false
 var charging = false
+var health : int = 2
 
 func _ready() -> void:
 	planet.PlanetCollision.connect(_planet_collision)
 
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = global_position.direction_to(planet.global_position)
-	velocity = direction * 150
+	velocity = direction * 75
 	move_and_slide()
 
 func _planet_collision(body):
@@ -20,8 +21,13 @@ func _planet_collision(body):
 		charging = true
 
 func on_hit() -> void:
-	wave_spawner.alien_death.emit()
-	queue_free()
+	health -= 1
+	#wave_spawner.alien_death.emit()
+	#queue_free()
+	
+	if health <= 0:
+		wave_spawner.alien_death.emit()
+		queue_free()
 
 
 func _on_timer_timeout() -> void:
