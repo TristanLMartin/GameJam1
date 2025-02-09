3024a7aa473a7622689d25299b90e3889bbdbfe0
Brandon Hofbauer <71418632+brandonhofbauer@users.noreply.github.com>
2025-02-08 20:37:05 -0800

    we're rich

--- gamejam1/main.gd
+++ gamejam1/main.gd
@@ -3,17 +3,18 @@ extends Node
 enum game_states {PAUSE_MENU, PLAYING, UPGRADING, START_MENU}
 var game_state = game_states.PLAYING
 
-@export var gold_total = 0
+@export var gold_total : int = 0
+@onready var gold_label = %Gold
 
 func _ready() -> void:
-	pass
+	gold_label.text = str("Gold: 0")
 
 #func _process(delta: float) -> void:
 	#print(game_state)
 
 func add_gold(gold) -> void:
 	gold_total += gold
-	print(gold_total)
+	gold_label.text = str('Gold: ', gold_total)
 	
 
 func _on_planet_planet_death() -> void:
