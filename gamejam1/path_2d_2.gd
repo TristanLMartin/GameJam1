3afeee2a67ea2d1874f1907b1c1d4a0ace1e9de2
TristanLMartin <tristanlmartin95@gmail.com>
2025-02-11 18:39:52 -0800

    Perfectly smooth circle B)

---
+++ gamejam1/path_2d_2.gd
@@ -0,0 +1,16 @@
+@tool
+extends Path2D
+
+@export var SIZE = 480
+@export var NUM_POINTS = 500
+
+
+func _ready() -> void:
+	print("hello")
+	curve = Curve2D.new()
+	for i in NUM_POINTS:
+		curve.add_point(Vector2(0, -SIZE).rotated((i / float(NUM_POINTS)) * TAU))
+
+	# End the circle.
+	curve.add_point(Vector2(0, -SIZE))
+	self.curve = curve
