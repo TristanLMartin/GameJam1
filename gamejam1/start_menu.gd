cffe0a076e38bdab9d14d404f23824576bc23743
TristanLMartin <tristanlmartin95@gmail.com>
2025-02-11 18:17:29 -0800

    Maybe got this to scale correctly?

--- gamejam1/start_menu.gd
+++ gamejam1/start_menu.gd
@@ -1,6 +1,15 @@
 extends Control
 
 @onready var main_music = get_node('/root/Main/MainMusic')
+var resolutions =[
+[1024,576],
+[1152,648],
+[1280,720],
+[1366,768],
+[1600,900],
+[1920,1080],
+[2560,1440],
+]
 
 func _on_button_pressed() -> void:
 	hide()
@@ -17,3 +26,7 @@ func _on_button_settings_pressed() -> void:
 
 func _on_music_slider_value_changed(value: float) -> void:
 	main_music.volume_linear = value * value * 0.001
+
+
+func _on_resolution_selected(index: int) -> void:
+	DisplayServer.window_set_size(Vector2(resolutions[index][0], resolutions[index][1]))
